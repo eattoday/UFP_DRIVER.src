b9610b56baf5073e813dcc7b0340e2010ddfbf6b
Huge_Sky <596906605@qq.com>
2017-12-05 20:44:07 +0800

    新增页面及工具
    SmsDuanxinServer.java
    1.新增的发送短信工具类
    2.需要添加jar包
    workOrderMonitor.jsp
    1.流程监控页面
    workOrderStart.jsp
    1.已发布工单页面

---
+++ main/java/com/metarnet/workflow/utils/SmsDuanxinServer.java
@@ -0,0 +1,57 @@
+package com.metarnet.workflow.utils;
+
+import com.eoms2.sms.adapter.RmiAdapter;
+import org.apache.log4j.Logger;
+
+import java.rmi.Naming;
+
+/**
+ * Title: eoms3<br>
+ * Description: <br>
+ * Copyright: Copyright 2017 <br>
+ * Create DateTime: Sep 19, 2017 10:31:06 AM <br>
+ * CVS last modify person: gpp <br>
+ * CVS last modify DateTime: <br>
+ * CVS last version: Revision: 1.0<br>
+ * 
+ * 
+ * @author gpp
+ */
+
+public class SmsDuanxinServer {
+
+
+	// 新增短信通知功能指定手机号和通知内容
+	public static void duanxinByphone(String phone, String returnXmlStr) {
+
+		try {
+			// 取得短信配置信息
+			String smsHost = "10.147.180.12:1334";
+			String smsSystem_id = "lizhidan";
+			String smsPassword = "lizhidan123";
+			if (1==1) {
+				RmiAdapter rmi = (RmiAdapter) Naming.lookup("rmi://" + smsHost + "/SmsRmiAdapter");
+				String tmp = "<root><username>u###</username><password>p###</password><message><address>a###</address><content>b###</content></message></root>";
+				String msg = returnXmlStr;
+				if (1==1) {
+					if (phone!=null&&!"".equals(phone)) {
+						// 填入短信服务用户名
+						tmp = tmp.replaceAll("u###", smsSystem_id);
+						// 填入短信服务密码
+						tmp = tmp.replaceAll("p###", smsPassword);
+						// 手机号
+						tmp = tmp.replaceAll("a###", phone);
+						// 内容
+						tmp = tmp.replaceAll("b###", msg);
+						rmi.sendSms(tmp);
+					}
+					tmp = "<root><username>u###</username><password>p###</password><message><address>a###</address><content>b###</content></message></root>";
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
